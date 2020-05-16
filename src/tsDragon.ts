a2ea7f88d73e36c32ca9fbbb6594ff7440918cdf
Tyreal Gray <tyrealgray@ymail.com>
2020-05-16 23:40:14 +0100

    update typescript support example

--- src/tsDragon.ts
+++ src/tsDragon.ts
@@ -1,11 +1,13 @@
 import * as PIXI from 'pixi.js';
+import Sprite = PIXI.Sprite;
+import Texture = PIXI.Texture;
 
 export class Dragon {
 
 	public sprite: PIXI.Sprite;
 
 	constructor(container){
-		this.sprite = new PIXI.Sprite(PIXI.Texture.from('../res/dragon.png'));
+		this.sprite = new Sprite(Texture.from('../res/dragon.png'));
 		this.sprite.anchor.x = 0.5;
 		this.sprite.anchor.y = 0.5;
 		this.sprite.x = 400;
